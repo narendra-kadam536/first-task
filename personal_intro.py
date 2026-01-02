aefb02763bc175b66ba4b88be6fa113a01eb4e62
Narendra kadam <narendrakadam536@gmail.com>
2026-01-02 18:30:03 +0530

    Create personal_intro.py

---
+++ personal_intro.py
@@ -0,0 +1,12 @@
+personal_intro.py
+   python
+# Personal Introduction Program
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your hobby: ")
+
+print("\n Welcome Message ")
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
+print(f"You love {hobby}. Keep learning and growing! ")
